7fc6daf33e380c7d32e3340f0b97d1d64d1b898f
Mohammed Eid <meid5170@gmail.com>
2026-06-18 22:30:36 +0300

    Add event search functionality

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,20 +1,48 @@
-import { Container, Typography, Grid } from "@mui/material";
+import { useState } from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  TextField,
+} from "@mui/material";
 
 import EventCard from "../../components/EventCard/EventCard";
 import { events } from "../../data/events";
 
+
 function Events() {
+  const [search, setSearch] =
+  useState("");
+  const filteredEvents =
+  events.filter((event) =>
+    event.title
+      .toLowerCase()
+      .includes(
+        search.toLowerCase()
+      )
+  );
   return (
     <Container sx={{ mt: 5 }}>
+      
       <Typography
         variant="h3"
         gutterBottom
       >
         All Events
       </Typography>
+      <TextField
+        fullWidth
+        label="Search Events"
+        variant="outlined"
+        sx={{ mb: 3 }}
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+      />
 
       <Grid container spacing={3}>
-        {events.map((event) => (
+        {filteredEvents.map((event) => (
           <Grid
             item
             xs={12}
